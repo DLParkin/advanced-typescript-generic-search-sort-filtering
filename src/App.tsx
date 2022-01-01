6acf07ab79092098f620cac73a45352a462951bb
David <davidsatwork2@hotmail.com>
2022-01-01 20:36:35 +0800

    adds sort by ascend and descend

--- src/App.tsx
+++ src/App.tsx
@@ -12,47 +12,64 @@ import { Widget } from "./components/Renderers/Widget";
 import { Person } from "./components/Renderers/Person";
 
 function App() {
+  const [showPeople, setShowPeople] = useState<boolean>(false);
+
   const [query, setQuery] = useState<string>("");
+
   const [widgetProperty, setWidgetProperty] = useState<IProperty<IWidget>>({
     property: "title",
+    isDescending: true,
   });
+
   const [personProperty, setPersonProperty] = useState<IProperty<IPerson>>({
     property: "firstName",
+    isDescending: true,
   });
+
   return (
     <>
+      <button onClick={() => setShowPeople(!showPeople)}>
+        {showPeople ? "Show Widget" : "Show People"}
+      </button>
       <SearchInput setSearchQuery={setQuery} />
-      <h2>Widgets:</h2>
-      <SortInput
-        object={widgets[0]}
-        setProperty={(property) => setWidgetProperty({ property })}
-      />
-      {widgets
-        .filter((widget) =>
-          genericSearch(widget, ["title", "description"], query, false)
-        )
-        .sort((a, b) => genericSort(a, b, widgetProperty.property))
-        .map((widget, index) => {
-          return <Widget {...widget} />;
-        })}
-      <h2>People:</h2>
-      <SortInput
-        object={people[0]}
-        setProperty={(property) => setPersonProperty({ property })}
-      />
-      {people
-        .filter((person) =>
-          genericSearch(
-            person,
-            ["firstName", "lastName", "eyeColor"],
-            query,
-            false
-          )
-        )
-        .sort((a, b) => genericSort(a, b, personProperty.property))
-        .map((person, index) => {
-          return <Person {...person} />;
-        })}
+      {!showPeople ? (
+        <>
+          <h2>Widgets:</h2>
+          <SortInput
+            object={widgets[0]}
+            setProperty={(propertyType) => setWidgetProperty(propertyType)}
+          />
+          {widgets
+            .filter((widget) =>
+              genericSearch(widget, ["title", "description"], query, false)
+            )
+            .sort((a, b) => genericSort(a, b, widgetProperty))
+            .map((widget, index) => {
+              return <Widget {...widget} />;
+            })}
+        </>
+      ) : (
+        <>
+          <h2>People:</h2>
+          <SortInput
+            object={people[0]}
+            setProperty={(propertyType) => setPersonProperty(propertyType)}
+          />
+          {people
+            .filter((person) =>
+              genericSearch(
+                person,
+                ["firstName", "lastName", "eyeColor"],
+                query,
+                false
+              )
+            )
+            .sort((a, b) => genericSort(a, b, personProperty))
+            .map((person, index) => {
+              return <Person {...person} />;
+            })}
+        </>
+      )}
     </>
   );
 }
--- src/components/SortInput.tsx
+++ src/components/SortInput.tsx
@@ -1,8 +1,9 @@
 import React from "react";
+import IProperty from "../interfaces/iProperty";
 
 export interface ISorterProps<T> {
   object: T;
-  setProperty: (property: keyof T) => void;
+  setProperty: (propertyType: IProperty<T>) => void;
 }
 
 export function SortInput<T>(props: ISorterProps<T>) {
@@ -15,13 +16,26 @@ export function SortInput<T>(props: ISorterProps<T>) {
       <select
         id="sorter"
         className="custom-select"
-        onChange={(e) => setProperty(e.target.value as any)}
+        onChange={(e) => {
+          const values = e.target.value.split("-");
+          if (values.length === 2) {
+            setProperty({
+              property: values[0] as any,
+              isDescending: values[1] === "true",
+            });
+          }
+        }}
       >
         {Object.keys(object).map((key) => {
           return (
-            <option key={key} value={key}>
-              {`Sort By ${key}`}
-            </option>
+            <>
+              <option key={`${key}-true`} value={`${key}-true`}>
+                {`Sort By ${key} descending`}
+              </option>
+              <option key={`${key}-false`} value={`${key}-false`}>
+                {`Sort By ${key} ascending`}
+              </option>
+            </>
           );
         })}
       </select>
